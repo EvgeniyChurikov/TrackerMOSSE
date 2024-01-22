0cdbe615047e449290bfc9c1e1c6113e5cb4c673
EvgeniyChurikov <zhenya.churikov@gmail.com>
2024-01-22 23:26:02 +0200

    Change approach for random affine

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.transforms as transforms
+from torchvision.transforms.functional import affine
 import torch.fft as fft
 
 
@@ -58,10 +59,17 @@ class Tracker:
     def _train(self, F, nu):
         A_new = 0
         B_new = 0
-        for _ in range(self.p):
-            Fp = transforms.RandomAffine(degrees=(-180 / 16, 180 / 16),
-                                         scale=(0.8, 1.2),
-                                         shear=(-10, 10))(F.unsqueeze(0)).squeeze(0)
+        angles = [5, -5, 10, -10, 20, -20, 30, -30, 45, -45, -60, 60]
+        shift = [[6, 6], [-6, 6], [6, -6], [-6, -6]]
+        angles_idx = torch.randint(len(angles), (self.p,))
+        shift_idx = torch.randint(len(shift), (self.p,))
+        scales = torch.FloatTensor(self.p).uniform_(0.8, 1.2)
+        for i in range(self.p):
+            Fp = affine(F.unsqueeze(0),
+                        angle=angles[angles_idx[i]],
+                        translate=[0, 0],
+                        scale=scales[i],
+                        shear=shift[shift_idx[i]]).squeeze(0)
             Fp = self._preprocess_box(Fp)
             Fp_ = fft.fft2(Fp)
             A_new += self.Gc_ * torch.conj(Fp_)
