8f43a99bd4f12d339f4abd5736933bb9ddd47fea
Yevhenii Churykov <yevhenii.churykov@gmail.com>
2024-01-23 14:47:00 +0200

    Add color support

--- tracker.py
+++ tracker.py
@@ -13,13 +13,13 @@ class Tracker:
         self.p = p
         self.nu = nu
         self.sigma = sigma
-        self.A = 0
-        self.B = 0
+        self.A = torch.zeros(3, height, width, dtype=torch.complex64)
+        self.B = torch.zeros(3, height, width, dtype=torch.complex64)
         Gc = self._make_gaussian(self.height, self.width, self.sigma)
         Gc = (Gc - Gc.min()) / (Gc.max() - Gc.min())
         self.Gc_ = fft.fft2(Gc)
         self.mask = self._make_mask(self.height, self.width)
-        frame_pt = transforms.Grayscale()(transforms.ToTensor()(frame)).squeeze(0)
+        frame_pt = transforms.ToTensor()(frame)
         F = self._get_box(frame_pt)
         self._train(F, 1.0)
 
@@ -50,15 +50,16 @@ class Tracker:
         return shift_y, shift_x
 
     def _get_box(self, frame_pt):
-        return frame_pt[self.top:self.top + self.height, self.left:self.left + self.width]
+        return frame_pt[:, self.top:self.top + self.height, self.left:self.left + self.width]
 
     def _preprocess_box(self, F):
-        std, mean = torch.std_mean(F)
-        return ((F - mean) / (std + 1E-8)) * self.mask
+        std, mean = torch.std_mean(F, dim=(-2, -1))
+        return ((F - mean[:, None, None]) / (std[:, None, None] + 1E-8)) * self.mask
 
     def _train(self, F, nu):
-        A_new = 0
-        B_new = 0
+        _, height, width = F.shape
+        A_new = torch.zeros(3, height, width, dtype=torch.complex64)
+        B_new = torch.zeros(3, height, width, dtype=torch.complex64)
         angles = [5, -5, 10, -10, 20, -20, 30, -30, 45, -45, -60, 60]
         blurs = [(3, 0.2), (5, 2), (3, 1), (7, 3), (5, 2)]
         shift = [[6, 6], [-6, 6], [6, -6], [-6, -6]]
@@ -67,14 +68,14 @@ class Tracker:
         shift_idx = torch.randint(len(shift), (self.p,))
         scales = torch.FloatTensor(self.p).uniform_(0.8, 1.2)
         for i in range(self.p):
-            Fp = affine(F.unsqueeze(0),
+            Fp = affine(F,
                         angle=angles[angles_idx[i]],
                         translate=[0, 0],
                         scale=scales[i],
                         shear=shift[shift_idx[i]])
             Fp = transforms.GaussianBlur(
                 kernel_size=blurs[blurs_idx[i]][0],
-                sigma=blurs[blurs_idx[i]][1])(Fp).squeeze(0)
+                sigma=blurs[blurs_idx[i]][1])(Fp)
             Fp = self._preprocess_box(Fp)
             Fp_ = fft.fft2(Fp)
             A_new += self.Gc_ * torch.conj(Fp_)
@@ -88,13 +89,13 @@ class Tracker:
         F_ = fft.fft2(F)
         W_ = self.A / (self.B + 1E-8)
         G_ = W_ * F_
-        G = fft.ifft2(G_).real
+        G = torch.sum(fft.ifft2(G_).real, dim=0)
         shift_y, shift_x = self._get_shift(G)
         self.top += shift_y
         self.left += shift_x
 
     def next(self, frame):
-        frame_pt = transforms.Grayscale()(transforms.ToTensor()(frame)).squeeze(0)
+        frame_pt = transforms.ToTensor()(frame)
         self._update_box(frame_pt)
         F = self._get_box(frame_pt)
         self._train(F, self.nu)
